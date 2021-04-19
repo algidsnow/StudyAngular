2ab173cebdefded369639091b08bed9214c6ccd6
HieuMD <HieuMd@Bagroup.com.vn>
2021-04-19 17:41:53 +0700

    Second push

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { MessageService } from '../message.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+selectedHero?: Hero;
+heroes:Hero[] = [];
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+}
+getHeroes(): void {
+  this.heroService.getHeroes()
+  .subscribe(heroes => this.heroes = heroes); 
+}
+
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
